8c60c6743446b93f3977ec808057af7ae2601f52
Mohammed M Rahman <mmrahman14@hotmail.com>
2020-07-05 16:10:35 +0100

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -15,15 +15,6 @@
 import axios from 'axios'
 
 export default {
-    asyncData ({ params, error }) {
-    return axios.get(`https://api.catsone.nl/v3/jobs`)
-      .then((res) => {
-        return { job: res.data }
-      })
-      .catch((e) => {
-        console.log(e)
-      })
-  },
   data() {
     return {
       
